92325258f8ff03dcf7af005cb12245a574559ca6
Johnny <johnnyhyman97@gmail.com>
2024-05-31 18:45:57 -0400

    ch3

--- ch3.hs
+++ ch3.hs
@@ -1,15 +1,24 @@
+--add :: Num a => (a,a) -> a
 add (x,y) = x+y
 
+add' :: Num a => a -> a -> a
 add' x y = x+y
 
+second :: [a] -> a
 second xs = head (tail xs)
 
+second' = (!!1)
+
+swap :: (b, a) -> (a, b)
 swap (x,y) = (y,x)
 
+pair :: a -> b -> (a, b)
 pair x y = (x,y)
 
+double :: Num a => a -> a
 double x = x*2
 
+palindrome :: Eq a => [a] -> Bool
 palindrome xs = reverse xs == xs
 
 twice f x = f (f x) 
