847a1e73d1a594c2ca5cdc5e5c5c41252e63a0ac
Johnny <johnnyhyman97@gmail.com>
2024-06-01 16:42:41 -0400

    ch5

--- ch4_base.hs
+++ ch4_base.hs
@@ -1,36 +1,59 @@
 --null'
---null' :: [a] -> Bool
+null' :: [a] -> Bool
 
---null' cond
+null' xs = if xs == [] then True else False
 
 
 --null'' pattern
+null'' :: [a] -> Bool
+null'' [] = True
+null'' _ = False
 
 --null''' guarded
-
+null''' xs
+	| xs == [] = True
+	| otherwise = False
 
 --safetail
 --safetail cond
 --safetail :: [a] -> [a]
+safetail xs = if null xs then [] else tail xs
 
 --safetail pattern
+safetail' [] = True
+safetail' xs = tail xs
 
 --safetail guarded
+safetail''' xs
+	| null xs = []
+	| otherwise = tail xs
 
---(||) :: Bool -> Bool -> Bool
+
 --explicit
+(|||) :: Bool -> Bool -> Bool
+False ||| False = False
+False ||| True = True
+True ||| False = True
+True ||| True = True
+
  --match both arguments
+(|||) :: Bool -> Bool -> Bool
+False ||| False = False
+_	  ||| _ = True
 
 --match first arg only
-
+(||||) :: Bool -> Bool -> Bool
+False |||| x = x
+_	  |||| _ = True
 
 -- && with cond
 {- True && True = True
    _    && _    = False -}
---(&&) :: Bool -> Bool -> Bool
-
+(&&&) :: Bool -> Bool -> Bool
+(&&&) x y = if x then if y then True else False else False
 
 --above, again, with slightly different logic
-
+(&&&&) :: Bool -> Bool -> Bool
+x &&&& y = if x then y else False
 
 
--- ch5_base.hs
+++ ch5_base.hs
@@ -1,7 +1,7 @@
 pyths :: Int -> [(Int,Int,Int)]
---pyths n = [ (x,y,z) | x <-[1..n], y<-[1..n], z<-[1..n], x^2 +y^2 == z^2] 
+pyths n = [ (x,y,z) | x <-[1..n], y<-[1..n], z<-[1..n], x^2 +y^2 == z^2] 
 
-
+pyths n = [ (x,y,z) | x <-[1..n], y<-[1..n], z<-[(max x y)..n], x^2 +y^2 == z^2] 
 
 --factors :: Int -> [Int]
 
