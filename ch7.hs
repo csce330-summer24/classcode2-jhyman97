8a44bfce3b32bef06a7f040e297d8f28e7bbca06
Johnny <JPHYMAN@email.sc.edu>
2024-06-10 15:55:28 -0400

    ch8

--- ch7.hs
+++ ch7.hs
@@ -1,8 +1,8 @@
-filtmap p f xs = [f x | x <- xs, p x]
+filtmap' p f xs = [f x | x <- xs, p x]
 
-filtmap' p f xs = map f (filter p xs)
+filtmap'' p f xs = map f (filter p xs)
 
-filtmap'' p f = (map f).(filter p)
+filtmap''' p f = (map f).(filter p)
 
 map' f xs = [ f x | x <- xs]
 
@@ -16,8 +16,8 @@ filter' p xs = [ x | x <- xs, p x]
 
 filter'' p xs = foldr (\x rest -> if p x then x:rest else rest) [] xs
 
-max' :: Ord a -> [a] -> a
+max' :: Ord a => [a] -> a
 max' (x:xs) = foldr (\y max_rest -> if y>max_rest then y else max_rest) x xs
 
-max'' :: Ord a -> [a] -> a
+max'' :: Ord a => [a] -> a
 max'' xs = foldr1 (\y max_rest -> if y>max_rest then y else max_rest) xs
--- ch8_base.hs
+++ ch8_base.hs
@@ -13,27 +13,45 @@ add :: Nat -> Nat -> Nat
 add Zero     n = n
 add (Succ m) n = Succ (add m n) 
 
--- mult :: Nat -> Nat -> Nat
-
+mult :: Nat -> Nat -> Nat
+mult Zero     n = Zero
+mult n Zero     = Zero
+mult (Succ m) n = add n (add m n)
 
 data Expr = Val Int
           | Add Expr Expr
           | Mul Expr Expr deriving Show
 
---folde :: (Int->Int) -> (Int->Int->Int) -> (Int->Int->Int) -> Expr -> Int
-
+folde :: (Int->Int) -> (Int->Int->Int) -> (Int->Int->Int) -> Expr -> Int
+folde vop _   _   (Val n) = vop n
+folde vop aop mop (Add x y) = aop fx fy
+    where
+        fx = folde vop aop mop x
+        fy = folde vop aop mop y
+folde vop aop mop (Mul x y) = mop fx fy
+    where
+        fx = folde vop aop mop x
+        fy = folde vop aop mop y
 
 
 data Tree a = Nil 
             | Leaf a
             | Node (Tree a) a (Tree a) deriving Show
 
--- complete :: Tree a -> Bool
+complete :: Tree a -> Bool
+complete Nil          = True
+complete (Leaf _)     = True
+complete (Node l _ r) = size l == size r && complete l && complete r
 
 
--- size :: Tree a -> Int
 
 
--- slide21 = Node (Node (Leaf 1)3(Leaf 4) ) 5 (Node (Leaf 6)7(Leaf 9))
+size :: Tree a -> Int
+size Nil           = 0
+size (Leaf _)     = 1
+size (Node l x r) = size l + 1 + size r
 
--- slide21incomplete = Node (Node (Leaf 1)3 Nil ) 5 (Node (Leaf 6)7(Leaf 9))
+
+slide21 = Node (Node (Leaf 1)3(Leaf 4) ) 5 (Node (Leaf 6)7(Leaf 9))
+
+slide21incomplete = Node (Node (Leaf 1)3 Nil ) 5 (Node (Leaf 6)7(Leaf 9))
